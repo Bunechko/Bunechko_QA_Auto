a98f5fa3560e1f834735854416f08a763e1fd5d3
Yurii Bunechko <byne4ko@gmail.com>
2023-10-19 14:44:05 +0300

    Added the test_github_api.py file and created the test_user_exists test

---
+++ tests/api/test_github_api.py
@@ -0,0 +1,8 @@
+import pytest
+from modules.api.clients.github import GitHub
+
+
+@pytest.mark.api
+def test_user_exists(github_api):
+    user = github_api.get_user("defunkt")
+    assert user["login"] == "defunkt"
