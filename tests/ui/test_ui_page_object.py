7d7e6ed6240ca6a555fec7095b4dbcef7abb7466
Yurii Bunechko <byne4ko@gmail.com>
2023-11-14 21:33:50 +0200

    Changed comments to English

--- tests/ui/test_ui_page_object.py
+++ tests/ui/test_ui_page_object.py
@@ -4,17 +4,17 @@ import pytest
 
 @pytest.mark.ui
 def test_check_incorrect_username_page_object():
-    # Створення об'єкту сторінки
+    # Creating a page object
     sign_in_page = SignInPage()
 
-    # Відкриваємо сторінку https://github.com/login
+    # We open the page https://github.com/login
     sign_in_page.go_to()
 
-    # Виконуємо спробу увійти в систему GitHub
+    # Trying to login to GitHub
     sign_in_page.try_login("Bunechko@not.exist", "bad_password")
 
-    # Перевіряємо, що назва сторінкитака, яку ми очікуємо
+    # We check that the name of the page is what we expect
     assert sign_in_page.check_title("Sign in to GitHub · GitHub")
 
-    # Закриваємо браузер
+    # Close the browser
     sign_in_page.close()
